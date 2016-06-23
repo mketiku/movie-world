1a3f208edc3359994d080bd246f9086164b5303a
michael ketiku <mketiku@gmail.com>
2016-06-23 14:09:15 -0400

    first step with DB files

---
+++ db/config.js
@@ -0,0 +1,12 @@
+// database stuff
+const dbServer = 'localhost';
+const database = 'movieworld';
+const user = 'postgres';
+const password = 'postgres';
+
+module.exports = {
+    dbServer,
+    database,
+    user,
+    password,
+};
